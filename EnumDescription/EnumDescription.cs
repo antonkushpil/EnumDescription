0b63168c23204c0609ea9a642879c1ef2958c5d2
Anton Kushpil <Anton_Kushpil@epam.com>
2016-12-16 15:00:37 +0200

    First commit

---
+++ EnumDescription/EnumDescription.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EnumDescription
+{
+	public class EnumDescription : Attribute
+	{
+		public string Description { get; }
+		public EnumDescription(string description)
+		{
+			Description = description;
+		}
+	}
+}
---
+++ EnumDescription/EnumExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EnumDescription
+{
+	public static class EnumExtension
+	{
+		public static T ParseEnum<T>(this string property)
+		{
+			var enumType = typeof(T);
+			if(!enumType.IsEnum) throw new ArgumentException("T must be an enumerated type");
+
+			foreach(var field in enumType.GetFields())
+			{
+				object[] attributes = field.GetCustomAttributes(false);
+				foreach(var attribute in attributes)
+				{
+					EnumDescription description = attribute as EnumDescription;
+					if(description != null)
+					{
+						if(description.Description == property)
+						{
+							return (T)field.GetValue(typeof(T));
+						}
+					}
+				}
+			}
+			throw new Exception($"Couldn't parse \"{property}\" parameter to \"{typeof(T)}\" Enum");
+		}
+	}
+}
